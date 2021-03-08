9222c09c70c228bfeb4568c118c775b2befc8c17
“Gayathiri” <gayathiri.asokan@ideas2it.com>
2021-03-08 21:37:12 +0530

    completion of calculator

--- calculator/js/calculator.js
+++ calculator/js/calculator.js
@@ -1,17 +1,5 @@
-// function getResult() {
-//     var numbers = document.getElementById("result-value").value;
-//     var evaluate = eval(numbers);
-//     document.getElementById("final-result").value = evaluate;
-// }
-// function getOperation(operator) {
-//             document.getElementById("result-value").value += operator;
-//             getResult(); 
-// }
-// function getClear() {
-//     document.getElementById("final-result").value = "";
-//     document.getElementById("result-value").value="";
-// }
 'use strict';
+//Function that Dynamically created buttons using javascript
 function codeButton() {
     for (var index = 0; index < 18; index++) {
         var array = ['%', '+/-', 'C', '/', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', ','];
@@ -29,22 +17,29 @@ function codeButton() {
     }
 }
 window.onload = codeButton;
+
+//Function that attach event handlers to the dynamically created elements.
 document.addEventListener('click', function (event) {
     if (event.target.value === 'C') {
         document.getElementById('result-value').value = '';
         document.getElementById('final-result').value = '';
+    } else if (event.target.value === '+/-') {       
+        document.getElementById('result-value').value += '*-1';
+        document.getElementById('final-result').value *= '-1';
     } else {
         if (event.target.value === 'x') {
-            document.getElementById('result-value').value  += '*';
+            document.getElementById('result-value').value += '*';
+        } else if (event.target.value === '=') {
+            document.getElementById('result-value').value += '';
         } else {
-        document.getElementById('result-value').value += event.target.value;
+            document.getElementById('result-value').value += event.target.value;
         }
         var variable = document.getElementById('result-value').value;
+
+         //function that evaluates the digit and return result using the Function-constructor:
         function calculate(fn) {
             return new Function('return ' + fn)();
         }
         document.getElementById('final-result').value = calculate(variable);
-        console.log(calculate(variable));
     }
 });
-
